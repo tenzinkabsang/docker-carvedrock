7ab5518f94c026ef4c052f2c85bc0bf7c21d813a
Tenzin Kabsang <tenzinkabsang@gmail.com>
2024-09-09 15:09:51 -0600

    changed weather

--- CarvedRock.Api/Controllers/WeatherController.cs
+++ CarvedRock.Api/Controllers/WeatherController.cs
@@ -8,7 +8,7 @@ namespace CarvedRock.Api.Controllers
     {
         private static readonly string[] Summaries =
         [
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Tenzin", "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         ];
 
         private readonly ILogger<WeatherController> _logger;
@@ -25,7 +25,8 @@ namespace CarvedRock.Api.Controllers
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                //Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = Summaries[index - 1]
             })
             .ToArray();
         }
